2c644a6332d9b3c8d3a4db3531e3febb08eb1730
Marcin Kass <marcin.kass10@gmail.com>
2019-02-10 20:25:29 +0100

    added Reverse Arra Task

---
+++ InterviewSolutions/ReverseArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewSolutions {
+    public class ReverseArray {
+        private string[] array;
+
+        public ReverseArray() {
+            MakeArray();
+            Array.Reverse(array);
+            GetArray();
+        }
+
+        public void MakeArray() {
+            Console.WriteLine("Reverse Array - First number of N elements - enter and then your numbers in one line (splited by spacebar)");
+            int N = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            IEnumerable<string> slice = input.Split(' ').Take(N);
+            array = slice.ToArray();
+        }
+
+        public void GetArray() {
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
