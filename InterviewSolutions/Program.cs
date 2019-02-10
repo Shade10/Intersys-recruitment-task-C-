b3885409611dc4752fb8f790100f3ca451e4ff3a
Marcin Kass <marcin.kass10@gmail.com>
2019-02-10 20:29:25 +0100

    executed all functions for shows - needed uncoment in Program.cs

--- InterviewSolutions/Program.cs
+++ InterviewSolutions/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace InterviewSolutions {
-    class Program {
+    partial class Program {
         static void Main(string[] args) {
+
+            //ReverseArray reverseArray = new ReverseArray();
+            //Palindrome palindrome = new Palindrome();
+            //Permutation permutation = new Permutation();
+            //ExistingPowersOfTwo existingPowersOfTwo = new ExistingPowersOfTwo();
+            //FindingPrimes findingPrimes = new FindingPrimes();
+
+            DigitSum commonDigit = new DigitSum();
+
+
         }
     }
 }
